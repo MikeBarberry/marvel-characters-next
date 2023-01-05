320300b3914c137e7f04a2860465af3a559a4a6e
MikeBarberry <52507419+MikeBarberry@users.noreply.github.com>
2023-01-04 18:20:04 -0800

    add next image component to characters card

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -1,5 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+const characterImageStyle = {
+  justifySelf: 'center',
+  height: 'auto',
+  maxHeight: '250px',
+  width: 'auto',
+  maxWidth: '250px',
+  borderRadius: '15px',
+};
+
+function CharacterImage({ thumbnail, name, toggleHidden }) {
+  return (
+    <div
+      onClick={toggleHidden}
+      className='hero-image-wrapper'>
+      <Image
+        src={thumbnail}
+        alt={name}
+        width={250}
+        height={250}
+        style={characterImageStyle}
+      />
+    </div>
+  );
+}
 
 export default function CharacterCard({ character, isHidden, toggleHidden }) {
   const { _id, name, thumbnail, description } = character;
@@ -11,26 +37,25 @@ export default function CharacterCard({ character, isHidden, toggleHidden }) {
       'characterInfo',
       JSON.stringify({ _id, name, thumbnail, description })
     );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [editButtonClicked]);
 
   return (
-    <div className='card' key={_id}>
+    <div
+      className='card'
+      key={_id}>
       <h2>{name}</h2>
-      <img
-        src={thumbnail}
-        alt={name}
-        className='hero-image'
-        onClick={toggleHidden}
-      ></img>
+      <CharacterImage
+        thumbnail={thumbnail}
+        name={name}
+        toggleHidden={toggleHidden}
+      />
       {isHidden ? null : <p className='description'>{description}</p>}
       <button
         className='button'
         onClick={() => {
           setEditButtonClicked(true);
           router.push('/edit');
-        }}
-      >
+        }}>
         Edit
       </button>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
-  /* assetPrefix: isProd
-    ? 'https://ekkow2ojlk.execute-api.us-west-2.amazonaws.com/main'
-    : undefined, */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   output: 'standalone',
 };
 
